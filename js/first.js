4dbfa7ecc58efddfe149dfe95da1a24fd9320f0c
Nagaraj Ravinuthala <nagarajr@nexaria.com>
2022-07-12 08:41:06 +0530

    new and changed files

---
+++ js/first.js
@@ -0,0 +1,18 @@
+var x = 5;
+var y = 10;
+var z = x * y;
+console.log(z);
+
+var person = {
+  pname: "Nagaraj",
+  age: 45,
+};
+
+console.log(person["pname"]);
+console.log(person.age);
+
+if (false) {
+  console.log("this is true");
+} else {
+  console.log("false");
+}

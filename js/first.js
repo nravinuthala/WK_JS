727c8836f879ca600a721b750fa609ee49f9fd0a
Nagaraj Ravinuthala <nagarajr@nexaria.com>
2022-07-12 08:42:01 +0530

    new and changed files

--- js/first.js
+++ js/first.js
@@ -1,18 +1,32 @@
-var x = 5;
-var y = 10;
-var z = x * y;
-console.log(z);
+// var x = 5;
 
-var person = {
-  pname: "Nagaraj",
-  age: 45,
-};
+// var y = 10;
+// var z = x * y;
+// console.log(z);
 
-console.log(person["pname"]);
-console.log(person.age);
+// var person = {
+//   pname: "Nagaraj",
+//   age: 45,
+//   profession: "trainer",
+// };
 
-if (false) {
-  console.log("this is true");
-} else {
-  console.log("false");
+// console.log(person["pname"]);
+// console.log(person.age);
+
+// if (false) {
+//   console.log("this is true");
+// } else {
+//   console.log("false");
+// }
+
+// about = `${person.pname} is ${person.age} years old ${person.profession}`;
+// console.log(about);
+
+function setVal() {
+  // alert();
+  var z = 6;
+  alert("hi");
+  document.getElementById("uname").value = "some value";
+  alert(document.getElementById("uname").value);
+  console.log(document.getElementById("uname").value);
 }
